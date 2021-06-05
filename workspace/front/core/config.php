aa78002e9dbc13c0c9615a3d2ce7b32f29362066
Антон Сметанин <foremost186@gmail.com>
2021-06-05 14:50:01 +0600

    - Change DB

--- workspace/front/core/config.php
+++ workspace/front/core/config.php
@@ -3,5 +3,5 @@
 define('GR_DB_NAME' ,'h-34568_g-engine');
 define('GR_DB_LOGIN','h-34568_g-engine');
 define('GR_DB_PASSWORD','5zDevBe2brgs');
-define('GR_DB_HOST','127.0.0.1');
+define('GR_DB_HOST','localhost:3306');
 /* T */
