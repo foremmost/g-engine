6f0ee65e71d8f00a39f9c023c4f143fa35711a2f
Антон Сметанин <foremost186@gmail.com>
2021-06-05 15:35:34 +0600

    - DB Fix

--- workspace/front/core/config.php
+++ workspace/front/core/config.php
@@ -3,5 +3,5 @@
 define('GR_DB_NAME' ,'h-34568_g-engine');
 define('GR_DB_LOGIN','h-34568_g-engine');
 define('GR_DB_PASSWORD','5zDevBe2brgs');
-define('GR_DB_HOST','localhost:3306');
+define('GR_DB_HOST','localhost');
 /* T */
